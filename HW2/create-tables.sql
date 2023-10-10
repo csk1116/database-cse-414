a3bd4e0d6762b6c8c36969e65a6ea0ac7897da85
csk1116 <eric.cs.kuo@gmail.com>
2023-10-09 20:29:57 -0700

    add hw2.

---
+++ HW2/create-tables.sql
@@ -0,0 +1,39 @@
+PRAGMA foreign_keys=ON;
+
+CREATE TABLE FLIGHTS (
+    fid INT PRIMARY KEY,
+    month_id INT REFERENCES MONTHS(mid),  -- 1-12
+    day_of_month INT,  -- 1-31 
+    day_of_week_id INT REFERENCES WEEKDAYS(did),  -- 1-7, 1 = Monday, 2 = Tuesday, etc
+    carrier_id VARCHAR(7) REFERENCES CARRIERS(cid), 
+    flight_num INT,
+    origin_city VARCHAR(34), 
+    origin_state VARCHAR(47), 
+    dest_city VARCHAR(34), 
+    dest_state VARCHAR(46), 
+    departure_delay INT, -- in mins
+    taxi_out INT,        -- in mins
+    arrival_delay INT,   -- in mins
+    canceled INT,        -- 1 means canceled
+    actual_time INT,     -- in mins
+    distance INT,        -- in miles
+    capacity INT, 
+    price INT            -- in $
+);
+
+CREATE TABLE CARRIERS (
+    cid VARCHAR(7) PRIMARY KEY,
+    name VARCHAR(83)
+);
+
+CREATE TABLE MONTHS (
+    mid INT PRIMARY KEY,
+    month VARCHAR(9)
+);
+
+CREATE TABLE WEEKDAYS (
+    did INT PRIMARY KEY,
+    day_of_week VARCHAR(9)
+);
+
+
---
+++ HW2/import-tables.sql
@@ -0,0 +1,5 @@
+.mode CSV
+.import /Users/CSK/sqlite/flight-dataset/flights-small.csv FLIGHTS
+.import /Users/CSK/sqlite/flight-dataset/carriers.csv CARRIERS
+.import /Users/CSK/sqlite/flight-dataset/months.csv MONTHS
+.import /Users/CSK/sqlite/flight-dataset/weekdays.csv WEEKDAYS
