6c6a15004d89455969f40dc40b9335310f5e8726
csk1116 <eric.cs.kuo@gmail.com>
2023-10-23 09:52:18 -0700

    q5

--- HW3/hw3-q5.sql
+++ HW3/hw3-q5.sql
@@ -2,15 +2,14 @@
 SELECT DISTINCT
     f1.origin_city AS city
 FROM FLIGHTS AS f1
-WHERE f1.origin_city != 'Seattle WA'
-      AND f1.origin_city NOT IN (
-                                    SELECT DISTINCT
-                                        f2.dest_city
-                                    FROM FLIGHTS AS f2,
-                                         FLIGHTS AS f3
-                                    WHERE f2.origin_city = f3.dest_city
-                                          AND f3.origin_city = 'Seattle WA'
-                                )
+WHERE f1.origin_city NOT IN (
+                                SELECT DISTINCT
+                                    f2.dest_city
+                                FROM FLIGHTS AS f2,
+                                     FLIGHTS AS f3
+                                WHERE f2.origin_city = f3.dest_city
+                                      AND f3.origin_city = 'Seattle WA'
+                            )
       AND f1.origin_city NOT IN (
                                     SELECT DISTINCT
                                         f4.dest_city
