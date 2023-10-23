493515a07e7eb5ebe3e878c20c033cb438290b04
csk1116 <eric.cs.kuo@gmail.com>
2023-10-23 10:31:42 -0700

    q3

--- HW3/hw3-q3.sql
+++ HW3/hw3-q3.sql
@@ -2,13 +2,13 @@ SELECT DISTINCT
     f.origin_city AS origin_city,
     100.0 * ISNULL(
             (
-                SELECT COUNT(fid)
+                SELECT COUNT(origin_city)
                 FROM FLIGHTS
                 WHERE 1.5 * 60 > actual_time
                       AND canceled = 0
                       AND origin_city = f.origin_city
                 GROUP BY origin_city
-            ), 0) / count(f.fid) AS percentage
+            ), 0) / count(f.origin_city) AS percentage
 FROM FLIGHTS AS f
 WHERE f.canceled = 0
 GROUP BY f.origin_city
